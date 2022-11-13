3017df535f20771a45c143f18fb6d977e55bd534
Clau005 <bardanclaudiu29@gmail.com>
2022-11-13 14:14:00 +0000

    This is to add the repository containing basic JavaScript and Html files to git

---
+++ Basic_Javascript_1.js
@@ -0,0 +1,9 @@
+alert("Hello, I am a simple alert. Click OK!");
+
+//Here is the code neede for Basic_HTML_2 file
+
+function my_first_function() {
+    var string = "Kiss me, I'm Irish";
+    var result = string.fontcolor("green");
+    document.getElementById("Irish").innerHTML = result
+}
